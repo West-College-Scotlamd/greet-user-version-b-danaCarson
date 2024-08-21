bdf519295274ad843e73b0200c7dc90a5e646675
danaCarson <31543644+danaCarson@users.noreply.github.com>
2024-08-21 13:19:07 +0000

    good

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # main.py
-
 def greet_user(name):
     # TODO: return a greeting string "Hello, <name>!"
+    if not name:
+        return "Hello, Stranger!"
+    return f"Hello, {name}!"
     pass
 
 if __name__ == "__main__":
